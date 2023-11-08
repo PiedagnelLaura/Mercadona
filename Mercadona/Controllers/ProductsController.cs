0cc4df91e9ba69f27b9aa3cb31c7cbef6aac1ba1
laura <laura@DESKTOP-IR4CGFN>
2023-11-08 13:13:00 +0100

    add log in productscontroller

--- Mercadona/Controllers/ProductsController.cs
+++ Mercadona/Controllers/ProductsController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Mercadona.Context;
 using Mercadona.Models;
 using Microsoft.AspNetCore.Authorization;
-using System.Diagnostics;
+
 namespace Mercadona.Controllers
 {
     [Authorize(Roles = "Administrator")]
